7878adba52ba8d5efafe6d10dbc2c1a8bfa07951
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-06-09 10:04:47 +0700

    setting routes diserver js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //parse application/json
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
- app.listen(3000, () => {
-    console.log(`Server started on port`);
- });
+//panggil routes
+var routes = require("./routes");
+routes(app);
+
+app.listen(3000, () => {
+  console.log(`Server started on port`);
+});
